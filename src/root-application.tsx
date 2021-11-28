5da20aea626b7930a02b6286e47ae58f2813a2a7
Hoàng Minh Thắng <hoangminhthang21051995@gmail.com>
2021-11-28 15:21:00 +0700

    Refactor: clean code

---
+++ src/root-application.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles/index.css';
+
+export default function RootApplication() {
+    return (
+        <div>
+            Hello world
+        </div>
+    );
+}
