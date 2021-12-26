23d55dfb3fca2fff60b31b5284ac884a68c6eecc
Hoàng Minh Thắng <hoangminhthang21051995@gmail.com>
2021-12-26 15:50:33 +0700

    Add: chakra UI

--- src/root-application.tsx
+++ src/root-application.tsx
@@ -3,7 +3,7 @@ import { ErrorBoundary } from './@foundation/components/error-boundary';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from 'app/home';
 import { About } from 'app/about';
-
+import { ChakraProvider } from '@chakra-ui/react'
 import './styles/index.css';
 import './styles/index.scss';
 import { AppWrapper } from 'base/component';
@@ -11,16 +11,18 @@ import { AppWrapper } from 'base/component';
 export default function RootApplication() {
     return (
         <ErrorBoundary>
-            <div className="app">
-                <BrowserRouter>
-                    <AppWrapper>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/about" element={<About />} />
-                        </Routes>
-                    </AppWrapper>
-                </BrowserRouter>
-            </div>
+            <ChakraProvider>
+                <div className="app">
+                    <BrowserRouter>
+                        <AppWrapper>
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/about" element={<About />} />
+                            </Routes>
+                        </AppWrapper>
+                    </BrowserRouter>
+                </div>
+            </ChakraProvider>
         </ErrorBoundary>
     );
 }
