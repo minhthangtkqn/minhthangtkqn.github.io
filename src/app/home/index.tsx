643fdb7ee411906642f697664f72617e688a2589
Hoàng Minh Thắng <hoangminhthang21051995@gmail.com>
2022-01-18 23:48:54 +0700

    Add: query & display data

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,38 +1,32 @@
-import React from 'react';
-import { Box, Image, Flex, Badge, Text } from '@chakra-ui/react';
-import { StarIcon } from '@chakra-ui/icons';
+import React, { useEffect, useState } from 'react';
+import { DataProfile } from '@foundation/utils/data-access';
+import { ProductItem } from './product-item';
+import { Box, Text } from '@chakra-ui/react';
 
 export function Home() {
+    const [productList, setProductList] = useState<any[]>([]);
+
+    async function getData() {
+        try {
+            const response = await DataProfile.Get('albums');
+            setProductList(Array.isArray(response.data) ? response.data : []);
+        } catch (error) {
+            console.log('error: ', error);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
-        <div>
-            <h2>
+        <Box p="5">
+            <Text fontSize="4xl">
                 HOME PAGE
-            </h2>
-            <Box p="5" ml="3.5" maxW="320px" borderWidth="1px">
-                <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
-                <Flex align="baseline" mt={2}>
-                    <Badge colorScheme="pink">Plus</Badge>
-                    <Text
-                        ml={2}
-                        textTransform="uppercase"
-                        fontSize="sm"
-                        fontWeight="bold"
-                        color="pink.800"
-                    >
-                        Verified &bull; Cape Town
-                    </Text>
-                </Flex>
-                <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
-                    Modern, Chic Penthouse with Mountain, City & Sea Views
-                </Text>
-                <Text mt={2}>$119/night</Text>
-                <Flex mt={2} align="center">
-                    <Box as={StarIcon} color="orange.400" />
-                    <Text ml={1} fontSize="sm">
-                        <b>4.84</b> (190)
-                    </Text>
-                </Flex>
-            </Box>
-        </div>
+            </Text>
+            <div style={{ display: 'flex', justifyContent: 'space-between', gap: 24, flexWrap: 'wrap' }}>
+                {productList.map(productItem => <ProductItem key={productItem?.id} productData={productItem} />)}
+            </div>
+        </Box>
     );
 };
---
+++ src/app/home/product-item/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Box, Image, Flex, Badge, Text } from '@chakra-ui/react';
+import { StarIcon } from '@chakra-ui/icons';
+
+type Props = {
+    productData: any;
+};
+
+export const ProductItem: React.FC<Props> = ({ productData }) => {
+    return (
+        <Box p="5" maxW="320px" borderWidth="1px">
+            <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+            <Flex align="baseline" mt={2}>
+                <Badge colorScheme="pink">Plus</Badge>
+                <Text
+                    ml={2}
+                    textTransform="uppercase"
+                    fontSize="sm"
+                    fontWeight="bold"
+                    color="pink.800"
+                >
+                    Verified &bull; Cape Town
+                </Text>
+            </Flex>
+            <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+                {productData?.title}
+            </Text>
+            <Text mt={2}>$119/night</Text>
+            <Flex mt={2} align="center">
+                <Box as={StarIcon} color="orange.400" />
+                <Text ml={1} fontSize="sm">
+                    <b>4.84</b> (190)
+                </Text>
+            </Flex>
+        </Box>
+    );
+};
