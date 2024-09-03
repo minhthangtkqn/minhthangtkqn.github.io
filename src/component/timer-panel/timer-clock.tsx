98375d696cb561a7ea5c09b7aefb7493f00c48e8
Thang Hoang <thang.hoang>
2024-09-03 10:07:18 +0700

    feat: start/pause timer

--- src/component/timer-panel/timer-clock.tsx
+++ src/component/timer-panel/timer-clock.tsx
@@ -1,10 +1,31 @@
-import React, { useState, forwardRef, useImperativeHandle } from 'react';
+import React, { useState, forwardRef, useImperativeHandle, useEffect } from 'react';
 import styled from "styled-components";
 import dayjs, { Dayjs } from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 
 const StyledTimerClock = styled.div`
+    font-size: var(--fs-8xl);
+    padding: var(--spacing-4xl);
+    border: var(--bd);
+    border-width: var(--spacing-xs);
+    border-radius: var(--br-max);
+    border-color: var(--main-primary);
+    background-color: var(--main-primaryLighter);
+    height: 10rem;
+    width: 10rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    &:hover {
+        filter: brightness(103%);
+    }
 `;
+
+const simpleTimeNumberFormat = (value: number) => {
+    return value >= 10 ? value : '0' + value;
+};
 
 const COUNT_DOWN = 20 * 60;
 type Props = {};
@@ -13,7 +34,8 @@ export type TimerClockRef = {
     pause: () => void;
 };
 export const TimerClock = forwardRef<TimerClockRef, Props>(({ }, ref) => {
-    const [tempCountdown, setTempCountdown] = useState(COUNT_DOWN);
+    const [countdownInSeconds, setCountdownInSeconds] = useState(COUNT_DOWN);
+    const [isRunning, setRunning] = useState(false);
     const [intervalInstance, setIntervalInstance] = useState<ReturnType<typeof setInterval>>();
 
     useImperativeHandle(ref, () => ({
@@ -22,17 +44,27 @@ export const TimerClock = forwardRef<TimerClockRef, Props>(({ }, ref) => {
     }));
 
     const handleStart = () => {
-        const interval = setInterval(() => setTempCountdown(prev => prev - 1), 1000);
+        setRunning(true);
+        console.log('🚀 ~ handleStart ~ handleStart:');
+        const interval = setInterval(() => setCountdownInSeconds(prev => prev - 1), 1000);
         setIntervalInstance(interval);
     };
 
     const handlePause = () => {
         clearInterval(intervalInstance);
+        setRunning(false);
     };
 
+    useEffect(() => {
+        console.log('🚀 ~ TimerClock ~ tempCountdown:', countdownInSeconds);
+    }, [countdownInSeconds]);
+
+    const remainingMinutes = Math.floor((countdownInSeconds % (60 * 60)) / 60);
+    const remainingSeconds = countdownInSeconds % 60;
+
     return (
-        <StyledTimerClock>
-            {/* {} */}
+        <StyledTimerClock onClick={() => isRunning ? handlePause() : handleStart()}>
+            {simpleTimeNumberFormat(remainingMinutes)}:{simpleTimeNumberFormat(remainingSeconds)}
         </StyledTimerClock>
     );
 });
