89c081afbbe6b17bf9b7ac81ac24f940e601bb75
Thang Hoang <thang.hoang>
2024-09-24 07:12:07 +0700

    feat: task item > crud

--- src/component/task-panel/index.tsx
+++ src/component/task-panel/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Button } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { Button, Empty, Input } from 'antd';
+import { CloseOutlined, PlusOutlined } from '@ant-design/icons';
+import { v4 as uuidv4 } from 'uuid';
+import { addTaskItem, getTaskList, removeTaskItem } from "@/util";
 
 const StyledTaskPanel = styled.div`
     height: 100%;
@@ -26,36 +28,62 @@ const StyledTaskPanel = styled.div`
             padding: var(--spacing-xs) var(--spacing-sm);
             border-radius: var(--br-sm);
             border: var(--bd);
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
         }
     }
 `;
 
-const demoTaskList = [
-    {
-        _id: 1,
-        name: 'init project',
-    },
-    {
-        _id: 2,
-        name: 'clone navbar UI',
-    },
-];
+type TaskItem = {
+    _id: string;
+    title: string;
+};
 
 export const TaskPanel: React.FC = () => {
-    const [taskList, setTaskList] = useState<any[]>(demoTaskList);
+    const [inputValue, setInputValue] = useState('');
+    const [taskList, setTaskList] = useState<TaskItem[]>(getTaskList());
+
+    const handleAddNewTask = (value: string) => {
+        addTaskItem({ _id: uuidv4(), title: value });
+        setTaskList(getTaskList());
+    };
+
+    const handleRemoveTask = (task_id: string) => {
+        removeTaskItem(task_id);
+        setTaskList(getTaskList());
+    };
 
     return (
         <StyledTaskPanel>
             <div className="task-list-header">Task</div>
             <div className="task-list">
-                <Button
+                {/* <Button
                     className="task-item-btn"
                     icon={<PlusOutlined />}
                     size="large"
                     type="dashed"
-                >Add Task</Button>
+                >Add Task</Button> */}
+                <Input
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Input & Enter to add new task"
+                    onPressEnter={(e) => {
+                        // console.log('🚀 ~ e:', (e.target as HTMLInputElement)?.value);
+                        const value = (e.target as HTMLInputElement)?.value?.trim();
+                        console.log('🚀 ~ onPressEnter ~ value:', value);
+                        if (value) {
+                            handleAddNewTask(value);
+                            setInputValue('');
+                        }
+                    }}
+                />
 
-                {taskList.map(item => <div key={item._id} className="task-item">{item.name}</div>)}
+                {taskList.map(item => <div key={item._id} className="task-item">
+                    {item.title}
+                    <Button type="link" danger icon={<CloseOutlined />} size="small" onClick={() => handleRemoveTask(item._id)} />
+                </div>)}
+                {taskList.length === 0 ? <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} /> : null}
             </div>
         </StyledTaskPanel>
     );
