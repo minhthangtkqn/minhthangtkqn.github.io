3a3f9399f9c51d4c27441fd66582f674943a2cd7
Thang Hoang <thang.hoang>
2024-10-05 17:13:09 +0700

    chore: update base url for deployment

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig(() => {
             alias: [
                 { find: '@/', replacement: path.resolve(__dirname, 'src/') },
             ],
-        },
-        base: 'focus-app'
+        }
     };
 });
