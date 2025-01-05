5b7c12e1e68466624d31078ed1c5f6f35e710413
Thang Hoang <thang.hoang>
2025-01-06 00:48:41 +0700

    chore: tweak UI

--- src/module/flashcard/list-panel/index.tsx
+++ src/module/flashcard/list-panel/index.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { FlashcardModuleParam } from "../model";
 import { useSearchParams } from "react-router-dom";
 import { Flashcard } from "@/__lib__/model";
+import { SyncOutlined } from "@ant-design/icons";
 
 const StyledFlashcardListContainer = styled.div`
     flex: 1;
@@ -15,12 +16,33 @@ const StyledFlashcardListContainer = styled.div`
 const StyledFlashcardList = styled.div`
     border: var(--bd);
     border-radius: var(--br);
-    padding: var(--spacing);
     display: flex;
     flex-direction: column;
-    row-gap: var(--spacing-sm);
     flex: 1;
     overflow-y: auto;
+
+    .list-header {
+        font-size: var(--fs-xl);
+        font-weight: bold;
+        text-transform: uppercase;
+        background-color: var(--main-primary);
+        color: var(--contrast-primary);
+        padding: var(--spacing-sm);
+        min-height: var(--min-height-header);
+        display: flex;
+        align-items: center;
+        column-gap: var(--spacing-sm);
+        cursor: pointer;
+    }
+
+    .list-body {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        overflow-y: auto;
+        row-gap: var(--spacing-sm);
+        padding: var(--spacing-sm);
+    }
 `;
 
 const StyledFlashcardItem = styled.div`
@@ -35,31 +57,42 @@ const StyledFlashcardItem = styled.div`
 
     .description {
         font-size: var(--fs-sm);
-        color: var(--main-tertiary);
+        color: var(--main-tertiaryDarker);
     }
 `;
 
 export const FlashcardListPanel = () => {
     const {
         data: flashcardList,
+        loading: flashcardListLoading,
+        refresh: refreshFlashcardList,
     } = useRequest<Flashcard[]>(QueryApi.Flashcard.list());
     const [, updateSearchParams] = useSearchParams();
 
     return (
         <StyledFlashcardListContainer>
             <StyledFlashcardList>
-                {flashcardList?.map(item => <StyledFlashcardItem
-                    key={item._id}
-                    onClick={() => {
-                        updateSearchParams(prev => {
-                            prev.set(FlashcardModuleParam.flashcardId, item._id);
-                            return prev;
-                        });
-                    }}
-                >
-                    <div className="title">{item.title}</div>
-                    <div className="description truncate">{item.description}</div>
-                </StyledFlashcardItem>)}
+                <div className="list-header" onClick={() => refreshFlashcardList()}>
+                    <div>Flashcard</div>
+                    <SyncOutlined
+                        className="reload-icon"
+                        spin={flashcardListLoading}
+                    />
+                </div>
+                <div className="list-body">
+                    {flashcardList?.map(item => <StyledFlashcardItem
+                        key={item._id}
+                        onClick={() => {
+                            updateSearchParams(prev => {
+                                prev.set(FlashcardModuleParam.flashcardId, item._id);
+                                return prev;
+                            });
+                        }}
+                    >
+                        <div className="title">{item.title}</div>
+                        <div className="description truncate">{item.description}</div>
+                    </StyledFlashcardItem>)}
+                </div>
             </StyledFlashcardList>
         </StyledFlashcardListContainer>
     );
