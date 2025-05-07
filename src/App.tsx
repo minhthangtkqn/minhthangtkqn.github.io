188f8137b0828626bc648305794e424b5aa7a82d
Thang Hoang <hoangminhthang21051995@gmail.com>
2025-05-07 07:15:51 +0700

    feat: error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import './App.scss';
 import './style/index.scss';
 import React from 'react';
-import { AppFooter, AppNavBar } from '@/component';
+import { AppFooter, AppNavBar, ErrorBoundary } from '@/component';
 import { BaseApplication } from "./application";
 import { SearchParamProvider } from "./util";
 // import { BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => {
     return (
-        <SearchParamProvider>
-            <div className="app-wrapper">
-                <AppNavBar />
-                <BaseApplication />
-                <AppFooter />
-            </div>
-        </SearchParamProvider>
+        <ErrorBoundary>
+            <SearchParamProvider>
+                <div className="app-wrapper">
+                    <AppNavBar />
+                    <BaseApplication />
+                    <AppFooter />
+                </div>
+            </SearchParamProvider>
+        </ErrorBoundary>
         // <BrowserRouter>
         // </BrowserRouter>
     );
